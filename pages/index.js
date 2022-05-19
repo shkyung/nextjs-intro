da8248932786c3a085d75a22c6f1743789755f4b
shkyung <sunny.kyung88@gmail.com>
2022-05-19 21:48:24 +0900

    Fetching Data, Redirect and Rewrite

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,54 @@
+import {useState, useEffect} from 'react'
 import SEO from '../components/SEO'
+
 export default function Home() {
+    const [movies, setMovies] = useState()
+
+    useEffect(() => {
+        (async () => {
+            const response = await fetch("/api/movies")
+            const data = await response.json()
+            console.error("data ", data)
+
+            const {results} = data
+            setMovies(results)
+        })()
+
+    }, [])
     return (
-        <>
+        <div className="container">
             <SEO title="Home"/>
-            <h1>Hello</h1>
-        </>
+            {!movies && <h4>Loading...</h4>}
+            {movies?.map((movie) => (
+                <div className="movie" key={movie.id}>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+                    <h4>{movie.original_title}</h4>
+                </div>
+            ))}
+            <style jsx>{`
+              .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 20px;
+                gap: 20px;
+              }
+
+              .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+              }
+
+              .movie:hover img {
+                transform: scale(1.05) translateY(-10px);
+              }
+
+              .movie h4 {
+                font-size: 18px;
+                text-align: center;
+              }
+            `}</style>
+        </div>
     )
 }
